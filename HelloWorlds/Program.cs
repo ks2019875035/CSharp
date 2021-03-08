84dcfd5a9c4e918323a1e2d9f724f49da2c0ca8c
ks2019875035 <48726384+ks2019875035@users.noreply.github.com>
2021-03-08 11:27:08 +0900

    Add files via upload

---
+++ HelloWorlds/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            //Console.ReadLine();
+            Console.WriteLine("2019875035 안혜지");
+        }
+    }
+}

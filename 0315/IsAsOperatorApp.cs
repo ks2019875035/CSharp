4471bddcdab8ba19d3a51134745dc9a94bd37e7f
ks2019875035 <48726384+ks2019875035@users.noreply.github.com>
2021-03-15 10:43:44 +0900

    Add files via upload

---
+++ 0315/IsAsOperatorApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsAsOperatorApp
+{
+    public class IsAsOperatorApp
+    {
+        static void IsOperator(object obj)
+        {
+            Console.WriteLine(obj + " is int : " + (obj is int));
+            Console.WriteLine(obj + " is string : " + (obj is string));
+        }
+        static void AsOperator(object obj)
+        {
+            Console.WriteLine(obj + " as string == null : " + (obj as string == null));
+        }
+        public static void Main()
+        {
+            IsOperator(10);
+            IsOperator("ABC");
+            AsOperator(10);
+            AsOperator("ABC");
+        }
+    }
+}
